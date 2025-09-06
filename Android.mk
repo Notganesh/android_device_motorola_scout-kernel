a40301ded4366cbf7a2023af166976d07fd1b0fb
Notganesh <chimeports@gmail.com>
2025-09-06 14:51:05 +0530

    scout:initializing kernel.binaries

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The Scout.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),scout)
+
+endif
---
+++ BoardConfigKernel.mk
@@ -0,0 +1,20 @@
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_KERNEL_PATH := device/motorola/scout-kernel
+
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := device/motorola/scout-kernel/kernel-headers
+TARGET_PREBUILT_KERNEL := $(DEVICE_KERNEL_PATH)/Image
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_KERNEL_CONFIG := gki_defconfig
+
+# DTB
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_KERNEL_PATH)/dtbs
+
+# DTBO
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_KERNEL_PATH)/dtbo.img
